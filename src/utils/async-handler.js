c2f02bf12322dad49a311ebe832dbce6171d80a3
Ansh Pandey <anshpandeyji02@gmail.com>
2026-01-02 19:53:18 +0530

    Updated comments

--- src/utils/async-handler.js
+++ src/utils/async-handler.js
@@ -1,6 +1,8 @@
 //To handle all async-await without using too many trycatch blocks. It auto handles catch part by using express's inbuilt error handler
 
-//Higher-order function that takes function as input and returns function as output
+//Higher-order function that takes function as input and returns function as output and does 2 things
+//1. Promise.resolve(input fuctn)    2. Promise.catch((err))
+
 const asyncHandler=(requestHandler) => {
     return (req,res,next) => {
         Promise                     //All the functions that you are assing are promisified=> No need trycatch anymore
